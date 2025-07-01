2a9e643a21fc009550bb50646e8a26f38634ebab
seongwonp <swp2013@naver.com>
2025-07-01 17:53:46 +0900

    해더파일 링크 수정

---
+++ src/main/java/com/opentime/smartparking_system/controller/contact/contactServlet.java
@@ -0,0 +1,17 @@
+package com.opentime.smartparking_system.controller.contact;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value="/contact/Q&A")
+public class contactServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/contact/contact.jsp").forward(req, resp);
+    }
+}
